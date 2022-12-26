8958e737d1cda3601d6f5b2a7286b955fc2bfdff
omokehinde <engr.omokehinde@gmail.com>
2022-12-26 23:11:48 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [url, setUrl] = useState("");
+  const [shortUrl, setshortUrl] = useState("");
+  const [shortUrl2, setshortUrl2] = useState("");
+  const [message, setMessage] = useState("");
+
+  let handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      let res = await fetch("https://api.shrtco.de/v2/", {
+        method: "POST",
+        body: JSON.stringify({
+          url: url,
+        }),
+      });
+      let resJson = await res.json();
+      if (res.ok === true) {
+        setshortUrl(res.result.short_link);
+        setshortUrl2(res.result.short_link2);
+      } else {
+        setMessage("Some error occured");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
+    <>
+    <h2 className="App">Create a shorter url for your website</h2>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={url}
+          placeholder="Type in Url"
+          onChange={(e) => setUrl(e.target.value)}
+        />
+
+        <button type="submit">Create</button>
+        <div>{shortUrl ? <><h4>Short Url:</h4> <p>{shortUrl}</p></> : null}</div>
+        <div>{shortUrl2 ? <><p><h4>Short Url2:</h4>{shortUrl2}</p></> : null}</div>
+        <div className="message">{message ? <p>{message}</p> : null}</div>
+      </form>
     </div>
+    </>
   );
 }
 
